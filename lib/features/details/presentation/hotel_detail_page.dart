68eab72fc1b08d931d905b3879044b585bc35ab3
v <victusves@gmail.com>
2025-06-09 20:33:32 +0330

    add hotel address and name

--- lib/features/details/presentation/hotel_detail_page.dart
+++ lib/features/details/presentation/hotel_detail_page.dart
@@ -3,6 +3,7 @@ import 'package:hoteli/core/utils/network.dart';
 import 'package:hoteli/features/home/data/models/hotel.dart';
 import 'package:hoteli/features/home/data/repositories/hotel_repositories.dart';
 import 'package:hoteli/shared/services/jason_data_service.dart';
+import 'package:persistent_bottom_nav_bar/persistent_bottom_nav_bar.dart';
 
 class HotelDetailPage extends StatelessWidget {
   const HotelDetailPage({super.key, required this.hotelId});
@@ -10,9 +11,9 @@ class HotelDetailPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
     final hotelRepositories =
         HotelRepositories(jsonDataService: JasonDataService());
-    final TextTheme = Theme.of(context).textTheme;
     return FutureBuilder<Hotel>(
       future: hotelRepositories.getHotelById(hotelId),
       builder: (context, snapshot) {
@@ -38,10 +39,52 @@ class HotelDetailPage extends StatelessWidget {
                     ),
                     onLongPress: () {},
                   ),
-                  title: Text(hotel.name),
                 ),
-                actions: const [],
+                elevation: 8,
+                expandedHeight: 300,
+                leading: IconButton(
+                  onPressed: () {
+                    PersistentNavBarNavigator.pop(context);
+                  },
+                  icon: const Icon(Icons.arrow_back, color: Colors.white),
+                ),
               ),
+              SliverToBoxAdapter(
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        hotel.name,
+                        style: textTheme.headlineMedium,
+                        textDirection: TextDirection.rtl,
+                      ),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Text(
+                            hotel.address,
+                            style: TextStyle(
+                                fontSize: 14, fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(
+                            width: 8,
+                          ),
+                          const Icon(
+                            Icons.location_on_outlined,
+                            size: 18,
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              )
             ],
           ),
         );
